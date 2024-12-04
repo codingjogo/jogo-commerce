479a309cdaf86b5c3670d5de7530ef271b615b7e
Joshua Gonzales <codingjogo@gmail.com>
2024-12-04 16:32:37 +0800

    feat: Product Gallery

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedProducts from "@/components/home/featured-products";
 import Hero from "@/components/home/hero";
+import ProductGallery from "@/components/home/product-gallery";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedProducts />
+      <ProductGallery />
     </>
   );
 }
