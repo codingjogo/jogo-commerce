613c3f067cd23f67d93f5169f4f3176e348ed12b
Joshua Gonzales <codingjogo@gmail.com>
2024-12-04 11:38:58 +0800

    feat: Add Onboarding

---
+++ lib/helpers.ts
@@ -0,0 +1,8 @@
+
+export function capitalizeWords(input: string) {
+  return input
+    .toLowerCase() // Convert the entire string to lowercase
+    .split(" ") // Split the string into an array of words
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+    .join(" "); // Join the words back into a single string
+}
